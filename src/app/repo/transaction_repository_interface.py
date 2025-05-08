1e9f7ef43bc67e6f5b606cb0eb1e32d1db40e870
Sakaef03 <fosakae@icloud.com>
2025-05-08 15:55:55 -0300

    feat: adiciona interface do repo da transacao

---
+++ src/app/repo/transaction_repository_interface.py
@@ -0,0 +1,38 @@
+import time
+from src.app.entities.transaction import Transaction
+from src.app.enums.transactions_type_enum import TransactionsType
+from src.app.repo.account_repository_mock import AccountRepositoryMock
+
+
+class Test_AccountRepositoryMock:
+
+    def test_get_account(self):
+
+        repo = AccountRepositoryMock()
+        account = repo.get_account(account_id=1)
+        assert account == repo.accounts.get(1)
+
+    def test_get_account_not_found(self):
+
+        repo = AccountRepositoryMock()
+        account = repo.get_account(account_id=10)
+        assert account is None
+
+    def test_make_deposit(self):
+
+        repo = AccountRepositoryMock()
+        account = repo.get_account(1)
+
+        repo.make_deposit(account, 200.0)
+
+        assert account.current_balance == 1200.0
+
+    def test_make_withdraw(self):
+
+        repo = AccountRepositoryMock()
+        account = repo.get_account(1)
+
+        repo.make_withdraw(account, 400.0)
+
+        assert account.current_balance == 600.0
+
